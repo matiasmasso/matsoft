29cca2107fb63807eda4cf8475da7475b152017d
matias <matias@matiasmasso.es>
2026-01-31 11:29:58 +0100

    menu

---
+++ Identity/Identity.Admin/Models/MenuModel.cs
@@ -0,0 +1,23 @@
+﻿namespace Identity.Admin.Models
+{
+    public class MenuModel
+    {
+        public List<Item> Items { get; set; } = new List<Item>();
+
+        public Item? SelectedItem { get; set; }
+
+        public void AddItem(string title, string url)
+        {
+            Items.Add(new Item
+            {
+                Title = title,
+                Url = url
+            });
+        }
+        public class Item
+        {
+            public string Title { get; set; } = string.Empty;
+            public string Url { get; set; } = string.Empty;
+        }
+    }
+}
